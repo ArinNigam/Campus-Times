6b09e51c62f3a90dcbd7ea073c8de9426c9b074c
Arin Nigam <arin.nigam@gmail.com>
2024-11-11 22:35:11 +0530

    Complete Backend

---
+++ backend/models/faculty.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const facultySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        default: 'Campus Times User'
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    cabinNumber: {
+        type: String,
+    },
+    department: {
+        type: String,
+    },
+    courses: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Course'
+    }],
+    
+});
+
+const Faculty = mongoose.model('Faculty', facultySchema);
+export default Faculty;
